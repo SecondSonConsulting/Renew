a7256dcfa3a821a3380ba4c21f732d3f3208f1d6
BigMac Admin <106293503+BigMacAdmin@users.noreply.github.com>
2023-10-30 08:57:14 -0700

    Update Renew-Report.sh
    Added current uptime reporting to Renew-Report.sh

--- Renew-Report.sh
+++ Renew-Report.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-set -x
+#set -x
 
 # Renew Report v1.0
 # Trevor Sysock aka Big Mac Admin
@@ -22,7 +22,9 @@ renewLogFile="${userHomeFolder}/Library/Application Support/Renew/Renew.log"
 
 renewUserDeferrals="${userHomeFolder}/Library/Preferences/com.secondsonconsulting.renew.user.plist"
 
-echo "Reading details for user: $currentUser"
+echo "****************************************************"
+echo "Deferral details for user: $currentUser"
+echo "****************************************************"
 
 if [ -f "$renewUserDeferrals" ]; then
     /usr/libexec/PlistBuddy -c "Print" "$renewUserDeferrals"
@@ -30,12 +32,38 @@ else
     echo "No user deferral file found"
 fi
 
-echo ""
 echo "****************************************************"
-echo ""
+echo "$lineCount lines of Renew logs for $currentUser:"
+echo "****************************************************"
 
 if [ -f "$renewLogFile" ]; then
     tail -n $lineCount "$renewLogFile"
 else
     echo "No Renew log files found"
 fi
+
+#Determine current Unix epoch time
+current_unix_time="$(date '+%s')"
+
+#This reports the unix epoch time that the kernel was booted
+boot_unix_time="$(sysctl -n kern.boottime | awk -F 'sec = |, usec' '{ print $2; exit }')"
+
+#Get uptime in seconds by doing maths
+uptime_seconds="$(( current_unix_time - boot_unix_time ))"
+
+#I'm spelling out the math in multiple steps because i'm kind of a dummy. This could be one unreadable command, but i prefer this.
+uptime_minutes="$(( uptime_seconds / 60 ))"
+uptime_hours="$(( uptime_minutes / 60 ))"
+uptime_days="$(( uptime_hours / 24 ))"
+
+echo "****************************************************"
+if [ $uptime_days -gt 0 ]; then
+    echo "Uptime is: $uptime_days days"
+elif [ $uptime_hours -gt 0 ]; then
+    echo "Uptime is: $uptime_hours hours"
+elif [ $uptime_minutes -gt 0 ]; then
+    echo "Uptime is: $uptime_minutes minutes"
+else
+    echo "Uptime less than 1 minute"
+fi
+echo "****************************************************"
